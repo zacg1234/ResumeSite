c653f0427d531d9b7b53a55644e7fbddf85c17e5
Zachary Gurevich <zachgurevich@gmail.com>
2021-01-30 19:52:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const EDUCATIONBTN = document.getElementById("education");
+const PROJECTBTN = document.getElementById("projects");
+const OTHERBTN = document.getElementById("other");
+const CONTACTBTN = document.getElementById("contact");
+
+const EDUTAG = document.getElementById("education-tag");
+const PROTAG = document.getElementById("project-tag");
+const OTHTAG = document.getElementById("other-tag");
+
+EDUCATIONBTN.addEventListener("click", scrollEDU);
+PROJECTBTN.addEventListener("click", scrollPRO);
+OTHERBTN.addEventListener("click", scrollOTH);
+
+
+function scrollEDU(){
+   // window.scrollBy(0, (window.innerHeight/100)*300);
+   EDUTAG.scrollIntoView();
+}
+
+function scrollPRO(){
+    PROTAG.scrollIntoView();
+}
+
+function scrollOTH(){
+    OTHTAG.scrollIntoView();
+}
+
+
+
+ 
